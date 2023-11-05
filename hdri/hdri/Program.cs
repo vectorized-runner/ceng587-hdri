dc8c1f5e9e12413fb356301103154da5f9e5d00e
Bartu Baş <bartubas99@gmail.com>
2023-11-05 18:07:18 +0300

    plot logic

--- hdri/hdri/Program.cs
+++ hdri/hdri/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ScottPlot;
 
 namespace HDRI
 {
@@ -13,11 +14,32 @@ namespace HDRI
             var runParameters = new RunParameters(random, sampleCountMultiplier: 1.0f, smoothnessFactor: 100.0f);
             var gFunctions = HDRIGenerator.SolveDebevec(runParameters, imageInfos);
 
+            var plt = new Plot();
+            var pixelValues = new double[256];
+            for (int i = 0; i <= 255; i++)
+            {
+                pixelValues[i] = i;
+            }
+
+            var red = Color.FromARGB((uint)System.Drawing.Color.Red.ToArgb());
+            var green = Color.FromARGB((uint)System.Drawing.Color.Green.ToArgb());
+            var blue = Color.FromARGB((uint)System.Drawing.Color.Blue.ToArgb());
+
+            plt.Add.Scatter(pixelValues, gFunctions.Red, red);
+            plt.Add.Scatter(pixelValues, gFunctions.Green, green);
+            plt.Add.Scatter(pixelValues, gFunctions.Blue, blue);
+
+            plt.Title("Line Plot with Custom Color");
+            plt.XLabel("Pixel");
+            plt.YLabel("g(Z)");
+
+            plt.SavePng("xd.png", 600, 600);
+
             foreach (var img in imageInfos)
             {
                 img.Dispose();
             }
-            
+
             Debug.Log("Program End");
         }
     }
