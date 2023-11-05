85896f6bba3beb45b920ef04fc20df5a947b26a7
Bartu Baş <bartubas99@gmail.com>
2023-11-05 16:55:58 +0300

    Reading exposure time is done

--- hdri/hdri/ImageReader.cs
+++ hdri/hdri/ImageReader.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Drawing;
 using System.IO;
+using System.Text;
+using ExifLib;
 
 namespace HDRI
 {
@@ -8,8 +10,8 @@ namespace HDRI
     {
         public static ImageInfo[] GetAllImagesAtPath(string relativeFolderName)
         {
-            var projectPath = AppDomain.CurrentDomain.BaseDirectory;
-            var absolutePath = Path.Combine(projectPath, relativeFolderName);
+            var exePath = AppDomain.CurrentDomain.BaseDirectory;
+            var absolutePath = Path.Combine(exePath, relativeFolderName);
             Debug.Log($"AbsolutePath for checking images: '{absolutePath}'");
 
             if (!Directory.Exists(absolutePath))
@@ -27,9 +29,23 @@ namespace HDRI
             {
                 var filePath = imageFiles[index];
                 result[index] = new ImageInfo(new Bitmap(filePath), 0.0f);
+
+                Debug.Log($"ExpTime: {ReadExposureTimeFromJPG(filePath)}");
             }
 
             return result;
+        }
+
+        // ReSharper disable once InconsistentNaming
+        private static float ReadExposureTimeFromJPG(string path)
+        {
+            using var reader = new ExifReader(path);
+            if (reader.GetTagValue(ExifTags.ExposureTime, out double exposureTime))
+            {
+                return (float)exposureTime;
+            }
+
+            throw new Exception($"Couldn't read exposure time from image: '{path}'");
         }
     }
 }
