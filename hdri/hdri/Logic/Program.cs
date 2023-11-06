9da85324f81d3d8dd1b9dd880befba03a1808169
Bartu Baş <bartubas99@gmail.com>
2023-11-06 09:46:13 +0300

    Benchmarking setup

--- hdri/hdri/Logic/Program.cs
+++ hdri/hdri/Logic/Program.cs
@@ -9,27 +9,40 @@ namespace HDRI
         {
             Debug.Log("Program Start");
 
+            var folderNames = new string[]
+            {
+                "Canon_EOS_550D",
+                "Minolta_DiMAGE_A1",
+                "Nikon_Coolpix_E5400",
+                "Nikon_D2H"
+            };
+
+            foreach (var folder in folderNames)
+            {
+                RunAlgorithm(folder);
+            }
+
+            //  GraphDrawer.Draw(cameraFolder, "Z", "g(Z)", $"{cameraFolder}.png", gFunctions);
+            Debug.Log("Program End");
+        }
+
+        private static void RunAlgorithm(string folder)
+        {
             var random = new Random(Seed: 1);
-            var cameraFolder = "Canon_EOS_550D";
-            var images = ImageReader.ExtractImageInfo($"exposure sequences/{cameraFolder}");
+            var images = ImageReader.ExtractImageInfo($"exposure sequences/{folder}");
             var runParameters = new RunParameters(random, sampleCountMultiplier: 1.0f, smoothnessFactor: 100.0f);
 
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-
             var gFunctions = HDRIGenerator.SolveDebevec(runParameters, images);
-
             stopwatch.Stop();
-            Debug.Log($"Algorithm took '{stopwatch.ElapsedMilliseconds}' ms");
 
-            GraphDrawer.Draw(cameraFolder, "Z", "g(Z)", $"{cameraFolder}.png", gFunctions);
+            Debug.Log($"Running '{folder}' took {stopwatch.ElapsedMilliseconds} ms.");
 
             foreach (var img in images)
             {
                 img.Dispose();
             }
-
-            Debug.Log("Program End");
         }
     }
 }
